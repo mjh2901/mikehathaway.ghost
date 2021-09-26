8f193404c59045e3e160a24ec16b32e873bab865
mjh2901 <mike@mikehathaway.com>
2021-09-26 10:17:58 -0700

    test

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@ gssg --domain "http://192.168.1.10:2368" --dest "docs"
 #read -p "Upload website changes via script" desc
 git add * && \
 git add -u && \
-git commit -m "autoupload changes via script"
+git commit -m "autoupload changes via publish.sh script"
 # git commit -m "$(read -p 'Upload website changes via script: ')" && \
 git push origin master
 
